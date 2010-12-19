3d208ffeae43811c8506b24bf08237d9bf4b6323
David Padbury <david@davidpadbury.com>
2010-12-19 03:30:38 -0500

    adding basic person model and images

---
+++ RazorExample/RazorExample/Models/Person.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace RazorExample.Models
+{
+    public class Person
+    {
+        public static IEnumerable<Person> ListAll()
+        {
+            return new[]
+                       {
+                           new Person
+                               {
+                                   Name = "David Padbury",
+                                   ImageUrl = "/Content/DavidPadbury.png"
+                               },
+                           new Person
+                               {
+                                   Name = "Scott Weinstein",
+                                   ImageUrl = "/Content/ScottWeinstein.png"
+                               },
+                           new Person
+                               {
+                                   Name = "Daniel Moore",
+                                   ImageUrl = "/Content/DanielMoore.png"
+                               }
+                       };
+        }
+
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
